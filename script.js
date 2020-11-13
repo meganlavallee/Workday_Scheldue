1d9be71a534d9fbd11a0ee6c088e09b0cbec9a53
Megan Lavallee <megan.lavallee@yahoo.com>
2020-11-13 10:28:03 -0700

    submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let currentDay = $("#currentDay").text(luxon.DateTime.local().toFormat("ff"));
-let currentTime = parseInt(luxon.DateTime.local().toFormat("H"));
+var d = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("demo").innerHTML = months[d.getMonth()];
+
+var d = new Date();
+document.getElementById("demo").innerHTML = d.getDate();
 
 for (let i = 9; i < 18; i++) {
     $(".container").append(buildTimeSlot(i));
 }
 
 
-
 const saveBtn = addLocalStorage
 localStorage.setItem('fas fa-save', 'value')
-    console.log("You Clicked")
+console.log("You Clicked")
+
+localStorage.setItem('fas fa-save', 'Tom');
+const cat = localStorage.getItem('fas fa save');
+
+$("#prospects_form").submit(function(e) {
+    e.preventDefault();
+});
